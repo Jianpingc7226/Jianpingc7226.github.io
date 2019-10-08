865b5f3aba9af0aede245874c9497bcfc25286dd
Jianpingc7226 <56267004+Jianpingc7226@users.noreply.github.com>
2019-10-08 11:47:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "JianpingC"
+let userName = "Jianpingc7226";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
